03dda36195dff459042ff2910066f594dbdb7720
lnmput <lnmput@gmail.com>
2023-12-07 16:29:35 +0900

    矩形个数随机指定

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def draw_square_border(draw, position, size, border_width):
         width=border_width
     )
 
-def generate_group_of_squares(image, square_size, border_width, area_position, area_size, area_color):
+def generate_group_of_squares(image, square_size, border_width, area_position, area_size, area_color, num_squares):
     """
     在指定区域内生成一组正方形边框
     """
@@ -31,24 +31,24 @@ def generate_group_of_squares(image, square_size, border_width, area_position, a
     x2, y2 = x1 + area_size[0], y1 + area_size[1]
     draw.rectangle([area_position, (x2, y2)], fill=area_color)
 
-    # 计算组内5个矩形边框组合排列到一起的长度以及高度
-    group_width = 5 * (square_size - border_width) + border_width
+    # 计算组内多个矩形边框组合排列到一起的长度以及高度
+    total_width = (square_size - border_width) * num_squares + border_width * (num_squares - 1)
     group_height = square_size
 
     # 计算在指定区域内的随机位置
-    x_position = random.randint(area_position[0], area_position[0] + area_size[0] - group_width)
+    x_position = random.randint(area_position[0], area_position[0] + area_size[0] - total_width)
     y_position = random.randint(area_position[1], area_position[1] + area_size[1] - group_height)
 
     # 绘制一组正方形边框
-    for i in range(5):
+    for i in range(num_squares):
+        if i > 0:
+            x_position += border_width  # 为了保持边框之间的间隔
         square_position = (x_position + i * (square_size - border_width), y_position)
         draw_square_border(draw, square_position, square_size, border_width)
 
-    return image
-
 if __name__ == "__main__":
     # 设置正方形边框的大小以及边框宽度
-    square_size = 50
+    square_size = 30
     border_width = 2
 
     # 生成A4大小的图片
@@ -59,8 +59,9 @@ if __name__ == "__main__":
     area_size = (400, 600)    # 指定区域的宽度和高度
     area_color = "lightgray"  # 指定区域的背景颜色
 
-    # 生成一组正方形边框在指定区域内随机放置
-    image_with_squares = generate_group_of_squares(a4_image, square_size, border_width, area_position, area_size, area_color)
+    # 生成3到8个正方形边框在指定区域内随机放置
+    num_squares = random.randint(3, 8)
+    generate_group_of_squares(a4_image, square_size, border_width, area_position, area_size, area_color, num_squares)
 
     # 显示生成的图片
-    image_with_squares.show()
+    a4_image.show()
