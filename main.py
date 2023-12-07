575fc2e32e413494afaede39b3f94e984ba8a560
lnmput <lnmput@gmail.com>
2023-12-07 15:53:31 +0900

    在指定区域内生成矩形框

--- main.py
+++ main.py
@@ -1,89 +1,66 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
+import random
 
-# 创建一个白色背景的A4图片
-width, height = 2480, 3508  # A4纸的宽度和高度，单位是像素
-background_color = (31, 41, 55)  # 白色
-image = Image.new("RGB", (width, height), background_color)
-draw = ImageDraw.Draw(image)
+def generate_blank_a4_image():
+    """
+    生成一个白色背景的A4纸大小的图片
+    """
+    width, height = 595, 842  # A4纸的大小
+    image = Image.new("RGB", (width, height), "white")
+    return image
 
-# 在图片内创建一个竖条的矩形框
-rectangle_width = 300  # 矩形框的宽度
-rectangle_height = height - 200  # 矩形框的高度，稍微小于A4高度，留出一些边距
-rectangle_x = 90  # 距离左侧3cm
-rectangle_y = 100  # 上下边距为100像素
-rectangle_color = (255, 255, 255)  # 彩色边框的颜色
-border_width = 5  # 矩形边框的宽度
-corner_radius = 20  # 圆角的半径
+def draw_square_border(draw, position, size, border_width):
+    """
+    在图片上绘制一个正方形边框
+    """
+    x, y = position
+    draw.rectangle(
+        [(x, y), (x + size, y + size)],
+        outline="black",
+        width=border_width
+    )
 
-draw.rectangle(
-    [
-        (rectangle_x, rectangle_y),
-        (rectangle_x + rectangle_width, rectangle_y + rectangle_height),
-    ],
-    outline=rectangle_color,
-    width=border_width,
-)
+def generate_group_of_squares(image, square_size, border_width, area_position, area_size, area_color):
+    """
+    在指定区域内生成一组正方形边框
+    """
+    draw = ImageDraw.Draw(image)
 
-# 在矩形框内绘制文字
-font_size = 58
-font = ImageFont.truetype("arial.ttf", font_size)
+    # 在指定区域绘制背景颜色
+    x1, y1 = area_position
+    x2, y2 = x1 + area_size[0], y1 + area_size[1]
+    draw.rectangle([area_position, (x2, y2)], fill=area_color)
 
-# 定义文字内容
-text_content = """
-1=A
-2=B
-3=C
-4=D
-5=E
-6=F
-7=G
-8=H
-9=I
-10=J
-11=K
-12=L
-13=M
-14=N
-15=O
-16=P
-17=Q
-18=R
-19=S
-20=T
-21=U
-22=V
-23=W
-24=X
-25=Y
-26=Z
-"""
+    # 计算组内5个矩形边框组合排列到一起的长度以及高度
+    group_width = 5 * (square_size - border_width) + border_width
+    group_height = square_size
 
-# 将文字按行分割
-lines = text_content.strip().split("\n")
+    # 计算在指定区域内的随机位置
+    x_position = random.randint(area_position[0], area_position[0] + area_size[0] - group_width)
+    y_position = random.randint(area_position[1], area_position[1] + area_size[1] - group_height)
 
-# 设置行高
-line_height = 1.5
+    # 绘制一组正方形边框
+    for i in range(5):
+        square_position = (x_position + i * (square_size - border_width), y_position)
+        draw_square_border(draw, square_position, square_size, border_width)
 
-# 计算每行文字的左上角位置
-start_x = rectangle_x + (rectangle_width - draw.textbbox((0, 0), lines[0], font=font)[2]) // 2  # 左右居中
+    return image
 
-# 计算总文字高度，考虑行间距
-total_text_height = sum(draw.textbbox((0, 0), line, font=font)[3] * line_height for line in lines)
+if __name__ == "__main__":
+    # 设置正方形边框的大小以及边框宽度
+    square_size = 50
+    border_width = 2
 
-# 计算 start_y，考虑矩形高度
-start_y = rectangle_y + (rectangle_height - total_text_height) // 2  # 上下居中
+    # 生成A4大小的图片
+    a4_image = generate_blank_a4_image()
 
-# 修改文字颜色为黑色
-text_color = (255, 255, 255)
+    # 设置指定区域的位置、大小和背景颜色
+    area_position = (50, 50)  # 从左上角开始
+    area_size = (400, 600)    # 指定区域的宽度和高度
+    area_color = "lightgray"  # 指定区域的背景颜色
 
-# 逐行绘制文字
-for line in lines:
-    text_width, text_height = draw.textbbox((0, 0), line, font=font)[2], draw.textbbox((0, 0), line, font=font)[3]
-    draw.text((start_x, start_y), line, font=font, fill=text_color)
-    start_y += text_height * line_height  # 使用text_height和line_height确保文字行间距正确
+    # 生成一组正方形边框在指定区域内随机放置
+    image_with_squares = generate_group_of_squares(a4_image, square_size, border_width, area_position, area_size, area_color)
 
-# 保存图片
-image.save("output_image.png")
-
-# 显示图片
-image.show()
+    # 显示生成的图片
+    image_with_squares.show()
